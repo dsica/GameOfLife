7cea943f6f2b1b33eba169436c0f3132f3862075
David Sica <david@sufficientmagic.com>
2020-01-06 14:12:01 -0700

    Initial Commit

---
+++ GameOfLife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameOfLife
+//
+//  Created by David Sica on 1/6/20.
+//  Copyright © 2020 Sufficient Magic Software LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
